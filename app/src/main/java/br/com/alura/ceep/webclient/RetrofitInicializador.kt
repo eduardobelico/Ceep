5e9d9f1257e431180c4808d06062b729bab7ad28
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-05-02 12:44:53 -0300

    Implementando o logging interceptor. Criando um model do webclient compativel e uma property para buscar notas e alterando o codigo para usar o modelo.

--- app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
@@ -1,14 +1,25 @@
 package br.com.alura.ceep.webclient
 
 import br.com.alura.ceep.webclient.services.NotaService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class RetrofitInicializador {
 
+    val client by lazy {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://localhost:8080/")
+        .baseUrl("http://192.168.100.106:8080/")
         .addConverterFactory(MoshiConverterFactory.create())
+        .client(client)
         .build()
 
     val notaService = retrofit.create(NotaService::class.java)
